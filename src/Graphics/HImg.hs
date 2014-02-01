7535c2f8077eaf0dd5ec5676dcf0b8ce9074693f
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-02-01 04:49:23 +0000

    Run in main thread due to GTK issues

--- src/Graphics/HImg.hs
+++ src/Graphics/HImg.hs
@@ -2,37 +2,34 @@
 module Graphics.HImg (displayImage) where
 
 import Control.Concurrent
-import System.Exit
 import Graphics.UI.Gtk
-import Graphics.UI.Gtk.Gdk.GC
 import Graphics.UI.Gtk.Gdk.Events
 
 -- | Takes 'Image' and spawns a window which displays it. It does not do any
 -- scaling and such. You can kill the window with ‘q’.
 displayImage ∷ Image → IO ()
 displayImage img = do
-  initGUI
+  _ ← initGUI
   window ← windowNew
 
-  set window [ containerBorderWidth := 0, containerChild := img
-             , windowResizable := False ]
+  set window [ containerBorderWidth := 0
+             , containerChild := img
+             , windowResizable := False
+             ]
 
   -- We block main thread on this until something in the program
   -- decides that it's time to kill the main window and exit.
-  exitVar ← newEmptyMVar
 
-  mapM_ ($ window) [ (`onDestroy` putMVar exitVar ())
-                   , (`onKeyPress` keyPressedEvent exitVar)
+  mapM_ ($ window) [ (`onKeyPress` keyPressedEvent window)
+                   , (`onDestroy` mainQuit)
                    ]
+
   widgetShowAll window
 
   -- Fork the graphics stuff and block until user exits
-  forkIO mainGUI
-  takeMVar exitVar
-  widgetDestroy window
-  mainQuit
+  mainGUI
 
 -- | Handles key events for the window spawned by 'displayImage'.
-keyPressedEvent ∷ MVar () → Event → IO Bool
-keyPressedEvent m Key{eventKeyChar = Just 'q'} = putMVar m () >> return True
+keyPressedEvent ∷ Window → Event → IO Bool
+keyPressedEvent w Key{eventKeyChar = Just 'q'} = mainQuit >> return True
 keyPressedEvent _ _ = return False
