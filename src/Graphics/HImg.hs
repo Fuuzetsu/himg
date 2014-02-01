9ab417cf5c47fcf094fe3f7761b902f5e67be749
Mateusz Kowalczyk <fuuzetsu@fuuzetsu.co.uk>
2014-02-01 16:15:48 +0000

    Move most functionality to HImg
    This lets the library usage be a bit better than having to create own Image.

--- src/Graphics/HImg.hs
+++ src/Graphics/HImg.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 -- |
 -- Module      :  Graphics.HImg
 -- Copyright   :  (c) Mateusz Kowalczyk 2013
@@ -7,11 +9,24 @@
 -- Maintainer  :  fuuzetsu@fuuzetsu.co.uk
 -- Stability   :  experimental
 -- Portability :  portable
-module Graphics.HImg (displayImage) where
+module Graphics.HImg ( displayImage
+                     , printHelp
+                     , viewFromFile
+                     , viewFromLink
+                     , viewGuess
+                     ) where
 
-import Control.Concurrent
-import Graphics.UI.Gtk
-import Graphics.UI.Gtk.Gdk.Events
+import           Control.Exception          (catch)
+import           Data.ByteString.Lazy       (hPut)
+import           Graphics.UI.Gtk
+import           Graphics.UI.Gtk.Gdk.Events (Event(..))
+import           Network.HTTP.Conduit       (simpleHttp, HttpException)
+import           Network.URI                (isURI)
+import           System.Directory           (doesFileExist)
+import           System.Exit                (exitWith, ExitCode(ExitFailure))
+import           System.IO                  (hClose)
+import           System.IO.Temp             (withSystemTempFile)
+import           Text.Printf                (printf)
 
 -- | Takes 'Image' and spawns a window which displays it. It does not do any
 -- scaling and such. You can kill the window with ‘q’.
@@ -21,23 +36,66 @@ displayImage img = do
   window ← windowNew
 
   set window [ containerBorderWidth := 0
-             , containerChild := img
-             , windowResizable := False
+             , containerChild       := img
+             , windowResizable      := False
              ]
 
-  -- We block main thread on this until something in the program
-  -- decides that it's time to kill the main window and exit.
-
-  mapM_ ($ window) [ (`onKeyPress` keyPressedEvent window)
+  mapM_ ($ window) [ (`onKeyPress` keyPressedEvent)
                    , (`onDestroy` mainQuit)
                    ]
 
   widgetShowAll window
-
-  -- Fork the graphics stuff and block until user exits
   mainGUI
 
 -- | Handles key events for the window spawned by 'displayImage'.
-keyPressedEvent ∷ Window → Event → IO Bool
-keyPressedEvent w Key{eventKeyChar = Just 'q'} = mainQuit >> return True
-keyPressedEvent _ _ = return False
+keyPressedEvent ∷ Event → IO Bool
+keyPressedEvent Key{eventKeyChar = Just 'q'} = mainQuit >> return True
+keyPressedEvent _                            = return False
+
+-- | Prints the help message.
+printHelp ∷ IO ()
+printHelp = putStr . unlines $
+  [ "HImg: very simple image viewer"
+  , ""
+  , "-h          Prints this help."
+  , "-f <file>   Opens the specified file."
+  , "-u <url>    Opens the file from the specified url."
+  , "<link>      Tries to guess whether something's a file or url and opens it."
+  , ""
+  , "Example usage:"
+  , "himg -f /tmp/foo.jpg"
+  , "himg http://example.com/animation.gif"
+  , ""
+  , "To quit the viewer, press ‘q’."
+  ]
+
+-- | Opens 'FilePath' from a file. Exits with 2 if the file doesn't exist.
+viewFromFile ∷ FilePath → IO ()
+viewFromFile p = doesFileExist p >>= \case
+  True  → imageNewFromFile p >>= displayImage
+  False → printf "%s doesn't exist\n" p >> exitWith (ExitFailure 2)
+
+-- | Opens a passed in URL if possible. Exits with 2 if a download
+-- problem occurs.
+viewFromLink ∷ String → IO ()
+viewFromLink link =
+  if not $ isURI link
+   then printf "%s is not a valid link\n" link >> exitWith (ExitFailure 2)
+   else simpleHttp link >>= \bs → do
+          withSystemTempFile "himg.imgfile" $ \p h →
+            hPut h bs >> hClose h >> imageNewFromFile p >>= displayImage
+        `catch`
+        \(e ∷ HttpException) →
+          printf "Encountered a problem while downloading %s: %s\n"
+            link (show e)
+          >> exitWith (ExitFailure 2)
+
+-- | Attempts to guess whether the input is a file or a URL and opens the image
+-- appropriately. Exits with 3 if it can't decide what it's looking at.
+viewGuess ∷ String → IO ()
+viewGuess p = doesFileExist p >>= \case
+  True  → viewFromFile p
+  False → if isURI p
+           then viewFromLink p
+           else printf "Don't know how to open %s\n" p
+                >> exitWith (ExitFailure 3)
--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE UnicodeSyntax #-}
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 -- |
 -- Module      :  Main
 -- Copyright   :  (c) Mateusz Kowalczyk 2013
@@ -11,71 +10,14 @@
 -- Portability :  portable
 module Main where
 
-import           Control.Exception
-import qualified Data.ByteString.Lazy as L
-import           Graphics.HImg (displayImage)
-import           Graphics.UI.Gtk
-import           Network.HTTP.Conduit
-import           Network.URI (isURI)
+import           Graphics.HImg
 import           System.Environment (getArgs)
-import           System.Exit
-import           System.Directory (doesFileExist)
-import           System.IO
-import           System.IO.Temp
-import           Text.Printf (printf)
+import           System.Exit        (exitWith, ExitCode(ExitFailure))
 
 main ∷ IO ()
-main = do
-  getArgs >>= \case
-    "-h":_ → printHelp >> exitWith (ExitFailure 1)
-    ["-f", link] → viewFromFile link
-    ["-u", link] → viewFromLink link
-    [link] → viewGuess link
-    _ → printHelp >> exitWith (ExitFailure 1)
-
--- | Prints the help message.
-printHelp ∷ IO ()
-printHelp = putStr . unlines $
-  [ "HImg: very simple image viewer"
-  , ""
-  , "-h          Prints this help."
-  , "-f <file>   Opens the specified file."
-  , "-u <url>    Opens the file from the specified url."
-  , "<link>      Tries to guess whether something's a file or url and opens it."
-  , ""
-  , "Example usage:"
-  , "himg -f /tmp/foo.jpg"
-  , "himg http://example.com/animation.gif"
-  , ""
-  , "To quit the viewer, press ‘q’."
-  ]
-
--- | Opens 'FilePath' from a file. Exits with 2 if the file doesn't exist.
-viewFromFile ∷ FilePath → IO ()
-viewFromFile p = doesFileExist p >>= \case
-  True → imageNewFromFile p >>= displayImage
-  False → printf "%s doesn't exist\n" p >> exitWith (ExitFailure 2)
-
--- | Opens a passed in URL if possible. Exits with 2 if a download
--- problem occurs.
-viewFromLink ∷ String → IO ()
-viewFromLink link =
-  if not $ isURI link
-   then printf "%s is not a valid link\n" link >> exitWith (ExitFailure 2)
-   else simpleHttp link >>= \bs → do
-          withSystemTempFile "himg.imgfile" $ \p h →
-            L.hPut h bs >> hClose h >> imageNewFromFile p >>= displayImage
-        `catch`
-        \(e ∷ HttpException) →
-          printf "Encountered a problem while downloading %s: %s\n"
-            link (show e)
-          >> exitWith (ExitFailure 2)
-
--- | Attempts to guess whether the input is a file or a URL and opens the image
--- appropriately. Exits with 3 if it can't decide what it's looking at.
-viewGuess ∷ String → IO ()
-viewGuess p = doesFileExist p >>= \case
-  True → viewFromFile p
-  False → if isURI p
-          then viewFromLink p
-          else printf "Don't know how to open %s\n" p >> exitWith (ExitFailure 3)
+main = getArgs >>= \case
+  "-h":_       → printHelp >> exitWith (ExitFailure 1)
+  ["-f", link] → viewFromFile link
+  ["-u", link] → viewFromLink link
+  [link]       → viewGuess link
+  _            → printHelp >> exitWith (ExitFailure 1)
